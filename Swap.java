ba68c7561dafe9760014389c34076e32e146b60f
sachinsapkal7799 <141129631+sachinsapkal7799@users.noreply.github.com>
2023-09-12 16:29:26 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,21 @@
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20;
+		
+		//System.out.println("Enter two no")
+		//swap s=new swap();
+		a=a+b;
+		b=a-b;
+		a=a-b;
+	
+		
+		System.out.println("A="+a);
+		System.out.println("B="+b);
+		
+	}
+
+}
